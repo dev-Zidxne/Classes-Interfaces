5b2d0ca8e0eb9068ede506bd137ea39d1ca77761
Zidane Innis <zidaneinnis@gmail.com>
2023-03-25 21:45:06 -0500

    Optional parameter properties

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,45 @@
-interface Greetable {
-  name: string;
+// type AddFn = (a: number, b: number) => number;
+interface AddFn {
+  (a: number, b: number): number;
+}
 
+let add: AddFn;
+
+add = (a: number, b: number) => {
+  return a + b;
+};
+interface Named {
+  readonly name?: string;
+  outputName?: string;
+}
+
+interface Greetable extends Named {
   greet(phrase: string): void;
 }
 
 class Person implements Greetable {
-  name: string;
+  name?: string;
+  age = 30;
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
   }
   greet(phrase: string) {
-    console.log(phrase + " " + this.name);
+    if (this.name) {
+      console.log(phrase + " " + this.name);
+    } else {
+      console.log("Hi");
+    }
   }
 }
 
 let user1: Greetable;
 
-user1 = new Person("Zidane");
+user1 = new Person();
+
+// user1.name = "Zidane";
 
 console.log(user1);
 
